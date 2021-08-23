4f6dd403a3982d7a505384d30c908e7afae65548
caowq <154490984@qq.com>
2021-08-23 17:32:44 +0800

    stream_demo_01:test:提交humanReadableByteCount，人类可读的数据，字节转换 b->m

---
+++ stream_demo_01/src/main/java/com/example/test/stream1/Test1.java
@@ -0,0 +1,37 @@
+package com.example.test.stream1;
+
+/**
+ * @author caowq
+ * @date 2021-08-23 10:33
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("humanReadableByteCount(123456789L, false) = " + humanReadableByteCount(123456789L, true));
+    }
+
+    //
+
+    /**
+     * From: https://programming.guide/worlds-most-copied-so-snippet.html</br>
+     *
+     * @param bytes
+     * @param si
+     * @return
+     */
+    public static strictfp String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        long absBytes = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absBytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(absBytes) / Math.log(unit));
+        long th = (long) Math.ceil(Math.pow(unit, exp) * (unit - 0.05));
+        if (exp < 6 && absBytes >= th - ((th & 0xFFF) == 0xD00 ? 51 : 0)) exp++;
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+        if (exp > 4) {
+            bytes /= unit;
+            exp -= 1;
+        }
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+
+}
